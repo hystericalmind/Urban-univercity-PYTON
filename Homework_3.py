6c413330d4518e9e3c6077b4d168aba57db14f58
hystericalmind <164636418+hystericalmind@users.noreply.github.com>
2024-03-26 23:29:00 +0300

    Add files via upload

---
+++ Homework_3.py
@@ -0,0 +1,16 @@
+# user_input = input("Введите координаты цели")
+# map(int, input().split())
+# print(type (user_input))
+# print (user_input)
+
+
+
+my_string = str('Ни 4его не понятно, но очень интересно')
+
+print (len (my_string))
+
+print (my_string .upper())
+print (my_string .lower())
+print (my_string .replace(' ', ''))
+print (my_string[0])
+print (my_string[-1])
