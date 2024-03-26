850df261c52f992ac82407b40fb8f4f2284ca281
hystericalmind <164636418+hystericalmind@users.noreply.github.com>
2024-03-26 19:34:15 +0300

    Add files via upload

---
+++ homework_2.py
@@ -0,0 +1,15 @@
+
+name = "Александр"
+print ('Name :' + name)
+
+age = 35
+print ('Age:' + str(age))
+
+age = age + 1
+print ('New age:' +str(age))
+
+is_student = 1==1
+print ('is_student: ' + str(is_student))
+
+
+
