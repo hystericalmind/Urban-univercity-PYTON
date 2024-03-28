62b3c625de395bf3d8193d92fbafe4d6ae370e6f
hystericalmind <164636418+hystericalmind@users.noreply.github.com>
2024-03-28 21:27:22 +0300

    Add files via upload

---
+++ homewok_4.py
@@ -0,0 +1,7 @@
+immutable_var = 100, False, "WoW"
+print(immutable_var)
+
+# immutable_var[0] = 200      # объекты нельзя изменять в кортежах
+
+mutable_list = [50, True, 2.2, 'Blast']
+print(mutable_list)
